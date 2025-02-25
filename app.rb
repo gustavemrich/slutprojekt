627b9e2ed45d292c216a21099ff5c5945c2fe1ca
Gustav Emrich <gustavjemrich@gmail.com>
2025-02-25 09:18:38 +0100

    Skapad startsida och sida för bilar.

--- app.rb
+++ app.rb
@@ -2,4 +2,16 @@ require 'sinatra'
 require 'slim'
 require 'sqlite3'
 require 'sinatra/reloader'
-require 'becrypt'
+require 'bcrypt'
+
+get('/') do
+  slim('/start')
+end
+
+get('/signup') do
+  slim(:signup)
+end
+
+get('/start') do
+  slim(:start)
+end
